90307d672e8bcf408504a6d4526341d37c72e68c
Craig Conover <cvconover@gmail.com>
2021-12-04 15:04:49 -0700

    pnconfig-cli has single property and property filter capability; next - add user email input to be used as the ponconfig cmd line user

--- server.js
+++ server.js
@@ -17,17 +17,25 @@ app.get('/express_backend', (req, res, next) => {
 
 app.get('/pnconfig', async (req, res, next) => {
   console.log("req.query", req.query);
-  const result = await runPnConfig(req.query.subkey, req.query.prop);
+  const result = await runPnConfig(req.query);
 
   console.log("result", result);
   res.send(result);
 }); 
 
-const runPnConfig = async (subKey, prop) => {
+const runPnConfig = async (params) => {
   console.log("runPnConfig");
 
-  let pnconfigcmd = `/Users/pubnubcvconover/Developer/pubnub/gits/pnconfig-cli/pnconfig-cli.py --email craig@pubnub.com ${subKey}`;
-  if (prop != null) pnconfigcmd = pnconfigcmd + ` ${prop}`;
+  let pnconfigcmd = `/Users/pubnubcvconover/Developer/pubnub/gits/pnconfig-cli/pnconfig-cli.py --email craig@pubnub.com ${params.subkey}`;
+
+  if (params.prop != null && params.prop !== "") {
+    console.log("prop query", params.prop);
+    pnconfigcmd = pnconfigcmd + ` ${params.prop}`;
+  }
+  else if (params.filter !=null && params.filter !== "") {
+    console.log("filter query", params.filter);
+    pnconfigcmd = pnconfigcmd + ` | egrep '${params.filter}'`
+  }
 
   const { stdout, stderr } = await exec(pnconfigcmd, ["-i"]);
 
@@ -37,6 +45,11 @@ const runPnConfig = async (subKey, prop) => {
   }
   else {
     console.log("success");
+    if (params.filter !=null && params.filter !== "") {
+      const data = JSON.parse("{" + stdout.replace(/\s/g, "").slice(0, -1) + "}");
+      console.log("data", data);
+      return data;
+    }
     return stdout;
   }
 }
