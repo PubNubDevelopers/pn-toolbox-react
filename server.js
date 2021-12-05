20b36e46fbe2518c5e1200c805e1b08e647edc1d
Craig Conover <cvconover@gmail.com>
2021-12-05 16:07:35 -0700

    added try/catch to the pnconfig exec but it's not really providing any helpful insight when VPN is not enabled

--- server.js
+++ server.js
@@ -24,36 +24,42 @@ app.get('/pnconfig', async (req, res) => {
 const runPnConfig = async (params) => {
   console.log("runPnConfig");
 
-  let pnconfigcmd = `/Users/pubnubcvconover/Developer/pubnub/gits/pnconfig-cli/pnconfig-cli.py --email craig@pubnub.com ${params.subkey}`;
+  try {
+    let pnconfigcmd = `/Users/pubnubcvconover/Developer/pubnub/gits/pnconfig-cli/pnconfig-cli.py --email craig@pubnub.com ${params.subkey}`;
 
-  if (params.prop != null && params.prop !== "") {
-    console.log("prop query", params.prop);
-    pnconfigcmd = pnconfigcmd + ` ${params.prop}`;
-  }
-  else if (params.filter !=null && params.filter !== "") {
-    console.log("filter query", params.filter);
-    pnconfigcmd = pnconfigcmd + ` | egrep '${params.filter}'`
-  }
-
-  const { stdout, stderr } = await exec(pnconfigcmd, ["-i"]);
-
-  if (stderr) {
-    console.error("error");
-    return stderr;
-  }
-  else {
-    console.log("success");
-    if (params.prop !=null && params.prop !== "") {
-      const data = {"properties": JSON.parse(stdout) };
-      console.log("data", data);
-      return data;
+    if (params.prop != null && params.prop !== "") {
+      console.log("prop query", params.prop);
+      pnconfigcmd = pnconfigcmd + ` ${params.prop}`;
     }
     else if (params.filter !=null && params.filter !== "") {
-      const data = JSON.parse('{"properties":{' + stdout.replace(/\s/g, "").slice(0, -1) + '}}');
-      console.log("data", data);
-      return data;
+      console.log("filter query", params.filter);
+      pnconfigcmd = pnconfigcmd + ` | egrep '${params.filter}'`
     }
 
-    return stdout;
+    const { stdout, stderr } = await exec(pnconfigcmd, ["-i"]);
+
+    if (stderr) {
+      console.error("error");
+      return stderr;
+    }
+    else {
+      console.log("success");
+      if (params.prop !=null && params.prop !== "") {
+        const data = {"properties": JSON.parse(stdout) };
+        console.log("data", data);
+        return data;
+      }
+      else if (params.filter !=null && params.filter !== "") {
+        const data = JSON.parse('{"properties":{' + stdout.replace(/\s/g, "").slice(0, -1) + '}}');
+        console.log("data", data);
+        return data;
+      }
+
+      return stdout;
+    }
+  }
+  catch (error) {
+    console.error("error");
+    return error;
   }
 }
